246ded64bbee03765ce275c1bfa42aab227c1f3d
ravielze <ravielz20@gmail.com>
2020-08-13 02:17:42 +0700

    Completed model & UI

--- data/models.py
+++ data/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.core.validators import RegexValidator, MaxValueValidator, MinValueValidator
+from PIL import Image
 
-SEXES = (('FEMALE', 'Female'), ('MALE', 'Male'))
+SEXES = (('PEREMPUAN', 'Perempuan'), ('LAKI-LAKI', 'Laki-laki'))
 MAJOR = (('IF', 'Teknik Informatika'), ('STI', 'Sistem Teknologi Informasi'))
 
 class Person(models.Model):
@@ -11,9 +12,20 @@ class Person(models.Model):
     nim_tpb = models.IntegerField(default=16519599, validators=[MaxValueValidator(16519600), MinValueValidator(16519000)])
     nim_jurusan = models.IntegerField(default=13519999)
     panggilan = models.CharField(validators=[text_regex], max_length=30)
-    jenis_kelamin = models.CharField(max_length=6,choices=SEXES)
-    jurusan = models.CharField(max_length=2, choices=MAJOR)
-    image = models.ImageField(default = 'default.png', upload_to=userDirectory)
+    jenis_kelamin = models.CharField(max_length=9,choices=SEXES)
+    jurusan = models.CharField(max_length=3, choices=MAJOR)
+    image = models.ImageField(default = 'default.png', upload_to='async')
+    kelompok = models.IntegerField(default=0, validators=[MaxValueValidator(12), MinValueValidator(0)])
+
+    def save(self, force_insert=False, force_update=False, using=None,
+             update_fields=None):
+        super().save()
+
+        img = Image.open(self.image.path)
+        if img.height > 256 or img.width > 256:
+            output_size = (256, 256)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
 
     def update(self):
         self.save()

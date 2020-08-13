9f2c2075d848367224059e3f1df4fce2c3e66154
ravielze <ravielz20@gmail.com>
2020-08-13 20:55:14 +0700

    Final Project

--- project/urls.py
+++ project/urls.py
@@ -18,11 +18,13 @@ from django.contrib import admin
 from . import settings
 from data import views as dv
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('data.urls')),
-]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT})
+]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 handler404 = dv.handler404
 handler500 = dv.handler500
